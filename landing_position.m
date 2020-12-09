9df79c34506a6781abd4dbd65a748625a5c2d98a
Ian Eykamp <ieykamp@olin.edu>
2020-12-09 11:22:06 -0500

    wrap in function

--- landing_position.m
+++ landing_position.m
@@ -1,33 +1,34 @@
-nBounces = 3; % actually, the number of bounces is one less than this number
+function Pf = landing_position(N, nBounces, g, COR, V0, P0)
 
-N = [1; 1; 1];
-N = N ./ norm(N);
+    N = N ./ norm(N);
 
-g = 9.80;
-COR = 0.95;
+    g = 9.80;
+    COR = 0.95;
 
-V = zeros(3, nBounces);
-Vnew = zeros(3, nBounces);
-Vperps = zeros(3, nBounces);
-P = zeros(3, nBounces);
+    V = zeros(3, nBounces);
+    Vnew = zeros(3, nBounces);
+    Vperps = zeros(3, nBounces);
+    P = zeros(3, nBounces);
 
-V(:, 1) = [2; -2; 2];
-P(:, 1) = [10; 10; 10];
-
-
-for bnc = 2:nBounces
-
-    % discriminant = sqrt(V(bnc - 1) ^ 2 + 2 * N(3) * g * (P(:, bnc - 1).' * N'))
-    t = (-V(:, bnc - 1).' * N - sqrt((V(:, bnc - 1).' * N)^2 + 2 * N(3) * g * (P(:, bnc - 1).' * N))) / -(N(3) * g);
-
-    P(:, bnc) = P(:, bnc - 1) + V(:, bnc - 1) * t + [0; 0; -1/2 * g] * t^2;
-
-    Vnew(:, bnc - 1) = V(bnc - 1) + [0; 0; -g] * t
-
-    Vperp = dot(Vnew(:, bnc - 1), N) * N;
-    Vperps(:, bnc) = Vperp;
+    V(:, 1) = V0;
+    P(:, 1) = P0;
 
 
-    V(:, bnc) = Vnew(:, bnc - 1) - Vperp * (1 + COR);
+    nBounces = nBounces + 1; % actually, the number of bounces is one less than this number
+    for bnc = 2:nBounces
+
+        % discriminant = sqrt(V(bnc - 1) ^ 2 + 2 * N(3) * g * (P(:, bnc - 1).' * N'))
+        t = (-V(:, bnc - 1).' * N - sqrt((V(:, bnc - 1).' * N)^2 + 2 * N(3) * g * (P(:, bnc - 1).' * N))) / -(N(3) * g);
+
+        P(:, bnc) = P(:, bnc - 1) + V(:, bnc - 1) * t + [0; 0; -1/2 * g] * t^2;
+
+        Vnew(:, bnc - 1) = V(:, bnc - 1) + [0; 0; -g] * t
+
+        Vperp = dot(Vnew(:, bnc - 1), N) * N;
+        Vperps(:, bnc) = Vperp;
+
+
+        V(:, bnc) = Vnew(:, bnc - 1) - Vperp * (1 + COR);
+    end
+    Pf = P(:, end)
 end
-P
